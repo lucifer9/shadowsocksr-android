133385a2875131b72e211fb22756f427a682814c
lucifer9 <lucifer@koredump.org>
2017-06-03 08:48:04 +0800

    delete ss-tunnel

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ try ${ANDROID_NDK_HOME}/ndk-build -j2
 # copy executables
 rm -rf assets/arm64-v8a
 mkdir -p assets/arm64-v8a
-for app in pdnsd redsocks ss-local ss-tunnel tun2socks
+for app in pdnsd redsocks ss-local tun2socks
 do
     try mv libs/arm64-v8a/$app assets/arm64-v8a/
 done
